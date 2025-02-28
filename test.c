5e22c0641ae5b41a0a362d66edd0d82498a8f4bb
danielpedro1 <danielpedro519@gmail.com>
2025-02-28 12:46:07 -0300

    Adicona os arquivos test.c e teste2.c

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Ola turma de estrutura de dados!\n");
+    return 0;
+}
---
+++ teste2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Ola turma de estrutura de dados! Teste 2\n");
+    return 0;
+}
+
+// pass12#test
